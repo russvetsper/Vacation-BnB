a22931a0f95ebb97594a7ad436a16e53fee6013d
russ vetsper and vichitra pool <russ+vichitra@>
2016-10-26 14:36:59 -0700

    refactor app and add bootstrap

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -3,19 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   isImageShowing: false,
   actions: {
-    ImageShow: function() {
+    imageShow: function() {
       this.set('isImageShowing', true);
     },
-    imageHide:function() {
-      this.set('isImageShowing', false);
-    },
-    update(rental, params) {
-      this.sendAction('update', rental, params)
-    },
-    delete(rental) {
-      if (confirm('are you sure?')) {
-        this.sendAction('destroyRental', rental);
-      }
-    }
+    imageHide: function() {
+     this.set('isImageShowing', false);
+   }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -12,24 +12,6 @@ export default Ember.Route.extend({
       newRental.save();
       this.transitionTo('index');
       //save feature
-    },
-
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-      if(params[key]!==undefined) {
-      rental.set(key,params[key]);
-      }
-    });
-      rental.save();
-      this.transitionTo('index');
-    },
-
-    //update feature
-  destroyRental(rental) {
-    rental.destroyRecord();
-    this.transitionTo('index');
-
-    //Delete feature
     }
   }
 });
