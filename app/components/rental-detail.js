3df4f73c9c1a9911dc271a1728d47b4062b69f8c
russ vetsper and vichitra pool <russ+vichitra@>
2016-10-31 11:56:46 -0700

    add upvoting function

--- app/components/rental-detail.js
+++ app/components/rental-detail.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['rating:desc'],
+  sortedReviews: Ember.computed.sort('rental.reviews', 'sortBy'),
+
+
   actions: {
     delete(rental) {
       if (confirm('Are you sure you want to delete this rental?')) {
