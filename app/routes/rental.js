3113f053ce817215e7f0a3fb82fe9c91fba5e0ae
russ vetsper and vichitra pool <russ+vichitra@>
2016-10-31 09:56:21 -0700

    add glyphiconto reviews

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -20,13 +20,13 @@ export default Ember.Route.extend({
     },
 
     saveReview(params) {
-      var newReview = this.store.createRecord('review', params);
-      var rental = params.rental;
-      rental.get('reviews').addObject(newReview);
-      newReview.save().then(function() {
-        return rental.save();
-      });
-      this.transitionTo('rental', rental);
-    }
+        var newReview = this.store.createRecord('review', params);
+        var rental = params.rental;
+        rental.get('reviews').addObject(newReview);
+        newReview.save().then(function() {
+          return rental.save();
+        });
+        this.transitionTo('rental', rental);
+      }
   }
 });
